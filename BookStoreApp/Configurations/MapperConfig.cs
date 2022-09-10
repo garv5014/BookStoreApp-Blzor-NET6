566281effa5e3e3899f2305213467b2b5dec2673
garv5014 <garv5014@gmail.com>
2022-09-10 13:49:59 -0600

    Added more controllers and setup mapping for the book controller.

--- BookStoreApp/Configurations/MapperConfig.cs
+++ BookStoreApp/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp.Data;
 using BookStoreApp.Models.Author;
+using BookStoreApp.Models.Book;
 
 namespace BookStoreApp.Configurations
 {
@@ -11,6 +12,15 @@ namespace BookStoreApp.Configurations
             CreateMap<AuthorCreateDto, Author>().ReverseMap();
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
             CreateMap<AuthorReadOnlyDto, Author>().ReverseMap();
+
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
+            CreateMap<Book, BookReadOnlyDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FristName} {map.Author.LastName}" ))
+                .ReverseMap();
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FristName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
---
+++ BookStoreApp/Models/Book/BookCreateDto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace BookStoreApp.Models.Book
+{
+    public class BookCreateDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string? Title { get; set; }
+
+        [Required]
+        [Range(1800, int.MaxValue)]
+        public int Year { get; set; }
+
+        [Required]
+        public string? Isbn { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 10)]
+        public string? Summary { get; set; }
+
+        public string? Image {get; set;}
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public decimal? Price { get; set; } 
+    }
+}
